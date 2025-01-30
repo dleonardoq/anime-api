236875064a735e1584578a7d76973df70cee7251
leonardo <quevedoruiz41@gmail.com>
2025-01-29 21:22:36 -0500

    Fix environment variables error

--- src/Utils/db/api-init-db.js
+++ src/Utils/db/api-init-db.js
@@ -1,11 +1,11 @@
 /* eslint-disable no-undef */
 const dbName = process.env.DB_NAME
 const dbUser = process.env.DB_USER
-const dbPass = process.env.DB_PASWWORD
+const dbPass = process.env.DB_PASSWORD
 const dbCollection = process.env.DB_COLLECTION
 
 if (!dbName || !dbUser || !dbPass || !dbCollection) {
-  throw new Error('Faltan variables de entorno necesarias')
+  throw new Error('Error getting global variables in init db file')
 }
 
 db = db.getSiblingDB(dbName)
