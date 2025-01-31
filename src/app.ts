3e3ffb396fc65b0efb550bdbdd941285c0cb9a7f
leonardo <quevedoruiz41@gmail.com>
2025-01-31 09:03:34 -0500

    Create a request limiter

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,14 @@ import router from './modules/anime/index'
 import { errorHandler } from './modules/Middlewares/errorHandler'
 import { notFound } from './modules/Middlewares/notFound'
 import { dbConectionMiddleware } from './modules/Middlewares/dbConnection'
+import { limiter } from './modules/Middlewares/requestLimiter'
 
 export const app = express()
 const { routerAnime } = router
 
 app.use(json())
 app.use(dbConectionMiddleware)
+app.use(limiter)
 app.use('/api/anime', routerAnime())
 
 app.use(notFound)
