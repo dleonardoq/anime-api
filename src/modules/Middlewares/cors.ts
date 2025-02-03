afd696ebdea45981a8f05e05fc898eda5b4ec2f2
leonardo <quevedoruiz41@gmail.com>
2025-02-02 20:48:28 -0500

    Add log when cors is not allowed

--- src/modules/Middlewares/cors.ts
+++ src/modules/Middlewares/cors.ts
@@ -1,7 +1,8 @@
+import { logger } from '@common/Logger'
 import cors from 'cors'
 
 const ALLOWED_ORIGINS = [
-  'http://localhost:40059'
+  'http://localhost:40054'
 ]
 
 export const corsMiddleware = ({ allowedOrigins = ALLOWED_ORIGINS } = {}): ReturnType<typeof cors> => cors({
@@ -14,6 +15,7 @@ export const corsMiddleware = ({ allowedOrigins = ALLOWED_ORIGINS } = {}): Retur
       return callback(null, true)
     }
 
+    logger.error({ message: `Origin not allowed by CORS: ${origin as string}` })
     return callback(new Error('Not allowed by CORS'))
   }
 })
